942765c4ba41c97ff0bc3f4d6eed7182b5a66c52
hiromi suzuki <easy.breezy.beautifulday@gmail.com>
2015-03-25 16:08:04 +0900

    completed: https://docs.angularjs.org/tutorial/step_02

--- angular-phonecat/app/js/controllers.js
+++ angular-phonecat/app/js/controllers.js
@@ -1,3 +1,10 @@
 'use strict';
-
-/* Controllers */
+var phonecatApp = angular.module('phonecatApp', []);
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+	$scope.property = "スコープのプロパティーは自由に持てる模様";
+	$scope.phones = [
+     {'name':'Nexus S', 'snippet':'ねくさすエス'},
+     {'name':'Motorola XOOM with Wi-Fi', 'snippet':'次世代タブレットのWIFIつき'},
+     {'name':'Motorola XOOM', 'snippet':'次世代タブレット'}
+     ];
+});
--- angular-phonecat/test/unit/controllersSpec.js
+++ angular-phonecat/test/unit/controllersSpec.js
@@ -2,10 +2,15 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
+describe('PhoneListCtrl', function() {
 
-  it("should do something", function() {
-
-  });
+  beforeEach(module('phonecatApp'));
+  
+  it("should create Phones model with 3 phones", inject(function($controller) {
+	  var scope = {};
+	  var ctrl = $controller('PhoneListCtrl', {$scope:scope});
+	  expect(scope.phones.length).toBe(3);
+	  expect(scope.property).toBe("スコープのプロパティーは自由に持てる模様");
+  }));
 
 });
